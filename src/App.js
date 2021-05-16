e2107be3e60421ba4db8fec0fb7c1573ea68f146
Girish <girish@Girishs-MacBook-Air.local>
2021-05-16 18:56:37 +0530

    Updated project with routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route path="/homepage/" component={HomePage} />
     </div>
   );
 }
